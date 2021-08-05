6aaa1e92e6cad155ce7f6f2b3d823127d8be8c9e
PASCAL ALHADOUM <alhadoumpascal@gmail.com>
2021-08-05 02:02:56 +0100

    Récupération des dernières données climatiques

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -20,15 +20,56 @@ class HomePageController extends ApplicationController
      */
     public function index($siteId, EntityManagerInterface $manager): Response
     {
+        $climates = [];
+        $climatesOut = [];
+        $inTemp = 0.0;
+        $inHum = 0.0;
+        $outTemp = 0.0;
+        $outHum = 0.0;
         $site = $manager->getRepository('App:Site')->find(['id' => $siteId]);
         if ($site) {
             $zones = $manager->getRepository('App:Zone')->findBy(['site' => $site, 'type' => 'PUE Calculation']);
             $gens = $manager->getRepository('App:SmartMod')->findBy(['site' => $site, 'modType' => 'FUEL']);
             //dump($gens);
-            $climates = [];
+            //$climates = [];
             if (count($zones) > 0) {
                 foreach ($zones[0]->getSmartMods() as $smartMod) {
                     if ($smartMod->getModType() === 'Climate' && $smartMod->getSubType() === 'Indoor') $climates[] = $smartMod;
+                    else if ($smartMod->getModType() === 'Climate' && $smartMod->getSubType() === 'Outdoor') $climatesOut[] = $smartMod;
+                }
+
+                $LastIndoorData = $manager->createQuery("SELECT d.temperature AS temp, d.humidity AS hum
+                                                            FROM App\Entity\ClimateData d
+                                                            JOIN d.smartMod sm 
+                                                            WHERE sm.id IN (:smartMods)
+                                                            AND d.dateTime = (SELECT MAX(d1.dateTime) FROM App\Entity\ClimateData d1 WHERE d1.dateTime LIKE :nowDate)
+                                                                                                                                                                                                            
+                                                            ")
+                    ->setParameters(array(
+                        'smartMods'      => $climates,
+                        'nowDate'     => date("Y-m-d") . "%",
+                    ))
+                    ->getResult();
+                $LastOutdoorData = $manager->createQuery("SELECT d.temperature AS temp, d.humidity AS hum
+                                                            FROM App\Entity\ClimateData d
+                                                            JOIN d.smartMod sm 
+                                                            WHERE sm.id IN (:smartMods)
+                                                            AND d.dateTime = (SELECT MAX(d1.dateTime) FROM App\Entity\ClimateData d1 WHERE d1.dateTime LIKE :nowDate)
+                                                                                                                                                                                                            
+                                                            ")
+                    ->setParameters(array(
+                        'smartMods'      => $climatesOut,
+                        'nowDate'     => date("Y-m-d") . "%",
+                    ))
+                    ->getResult();
+
+                if (count($LastIndoorData) > 0) {
+                    $inTemp = $LastIndoorData[0]['temp'];
+                    $inHum = $LastIndoorData[0]['hum'];
+                }
+                if (count($LastOutdoorData) > 0) {
+                    $outTemp = $LastOutdoorData[0]['temp'];
+                    $outHum = $LastOutdoorData[0]['hum'];
                 }
             }
             return $this->render('home_page/homepage2.html.twig', [
@@ -36,6 +77,10 @@ class HomePageController extends ApplicationController
                 'zone' => count($zones) > 0 ? $zones[0]->getId() : 0,
                 'gen'   => count($gens) > 0 ? $gens[0]->getId() : 0,
                 'climate'   => count($climates) > 0 ? $climates[0] : 0,
+                'inTemp'    => $inTemp,
+                'inHum'    => $inHum,
+                'outTemp'    => $outTemp,
+                'outHum'    => $outHum,
             ]);
         } else {
             $sites = $manager->getRepository('App:Site')->findBy(['enterprise' => $this->getUser()->getEnterprise(), 'isPublic' => true]);
@@ -44,10 +89,45 @@ class HomePageController extends ApplicationController
                 $gens = $manager->getRepository('App:SmartMod')->findBy(['site' => $sites[0], 'modType' => 'FUEL']);
                 //dump($gens);
                 $zones = $manager->getRepository('App:Zone')->findBy(['site' => $sites[0], 'type' => 'PUE Calculation']);
-                $climates = [];
+                //$climates = [];
                 if (count($zones) > 0) {
                     foreach ($zones[0]->getSmartMods() as $smartMod) {
                         if ($smartMod->getModType() === 'Climate' && $smartMod->getSubType() === 'Indoor') $climates[] = $smartMod;
+                        else if ($smartMod->getModType() === 'Climate' && $smartMod->getSubType() === 'Outdoor') $climatesOut[] = $smartMod;
+                    }
+
+                    $LastIndoorData = $manager->createQuery("SELECT d.temperature AS temp, d.humidity AS hum 
+                                                            FROM App\Entity\ClimateData d
+                                                            JOIN d.smartMod sm 
+                                                            WHERE sm.id IN (:smartMods)
+                                                            AND d.dateTime = (SELECT MAX(d1.dateTime) FROM App\Entity\ClimateData d1 WHERE d1.dateTime LIKE :nowDate)
+                                                                                                                                                                                                            
+                                                            ")
+                        ->setParameters(array(
+                            'smartMods'      => $climates,
+                            'nowDate'     => date("Y-m-d") . "%",
+                        ))
+                        ->getResult();
+                    $LastOutdoorData = $manager->createQuery("SELECT d.temperature AS temp, d.humidity AS hum 
+                                                            FROM App\Entity\ClimateData d
+                                                            JOIN d.smartMod sm 
+                                                            WHERE sm.id IN (:smartMods)
+                                                            AND d.dateTime = (SELECT MAX(d1.dateTime) FROM App\Entity\ClimateData d1 WHERE d1.dateTime LIKE :nowDate)
+                                                                                                                                                                                                            
+                                                            ")
+                        ->setParameters(array(
+                            'smartMods'      => $climatesOut,
+                            'nowDate'     => date("Y-m-d") . "%",
+                        ))
+                        ->getResult();
+
+                    if (count($LastIndoorData) > 0) {
+                        $inTemp = $LastIndoorData[0]['temp'];
+                        $inHum = $LastIndoorData[0]['hum'];
+                    }
+                    if (count($LastOutdoorData) > 0) {
+                        $outTemp = $LastOutdoorData[0]['temp'];
+                        $outHum = $LastOutdoorData[0]['hum'];
                     }
                 }
                 return $this->render('home_page/homepage2.html.twig', [
@@ -55,6 +135,10 @@ class HomePageController extends ApplicationController
                     'zone' => count($zones) > 0 ? $zones[0]->getId() : 0,
                     'gen'   => count($gens) > 0 ? $gens[0]->getId() : 0,
                     'climate'   => count($climates) > 0 ? $climates[0] : 0,
+                    'inTemp'    => $inTemp,
+                    'inHum'    => $inHum,
+                    'outTemp'    => $outTemp,
+                    'outHum'    => $outHum,
                 ]);
             } else if (count($sites) > 0) {
                 return $this->render('home_page/homepage1.html.twig', [
