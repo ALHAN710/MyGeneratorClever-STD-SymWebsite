33115da408b3fb3ddf5c5ca591da1d807ca8122e
PASCAL ALHADOUM <alhadoumpascal@gmail.com>
2021-09-05 16:45:20 +0100

    Modification de la fenêtre de date par défaut pour l'historique des énergies du GE

--- public/pages/jquery.forms-advanced1.js
+++ public/pages/jquery.forms-advanced1.js
@@ -93,6 +93,30 @@
             }
         }, cb);
 
+        start = moment().subtract(6, 'days');
+        end = moment();
+
+        function cb(start, end) {
+            $('#gendaterange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        }
+
+        $('#gendaterange').daterangepicker({
+            startDate: start,
+            endDate: end,
+            maxSpan: {
+                "days": 31
+            },
+            maxDate: moment().endOf('month'),
+            ranges: {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            }
+        }, cb);
+
         // Select2
         /*   $(".select2").select2({
                width: '100%'
